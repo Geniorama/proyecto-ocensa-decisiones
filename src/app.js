c3c53c51888f192b77c9e6f031bc8690946051ee
Geniorama <geniorama.info@gmail.com>
2021-02-28 17:49:14 -0500

    Instalación de dependencias

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// Libs
+import $ from "jquery";
+import 'bootstrap';
+import AOS from 'aos';
+import "./../node_modules/slick-carousel";
+import SmoothScroll from "smooth-scroll";
+
+// Fonts
+import "./fonts/Cabin-Regular.ttf";
+import "./fonts/Roboto-Bold.ttf";
+import "./fonts/Roboto-BoldItalic.ttf";
+import "./fonts/Roboto-Light.ttf";
+import "./fonts/Roboto-Medium.ttf";
+import "./fonts/Roboto-MediumItalic.ttf";
+import "./fonts/Roboto-Regular.ttf";
+
+// Styles
+import "./styles/app.sass";
