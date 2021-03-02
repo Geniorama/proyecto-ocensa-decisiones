497c2633e0ba9c118581f1299fc30166cf6e4a23
DuvanCoder <duvanprogrammer@gmail.com>
2021-03-02 17:00:31 -0500

    Adelantos home

--- src/app.js
+++ src/app.js
@@ -21,6 +21,8 @@ import "./fonts/Roboto-Regular.ttf";
 import "./styles/app.sass";
 
 // Images
+import ArrowLeft from "./img/left-arrow.svg";
+import ArrowRight from "./img/right-arrow.svg";
 import "./img/logo-ocensa.svg";
 import "./img/icon_menu_buscador_1.svg";
 import "./img/sprite-01-menu-blanco.svg";
@@ -29,5 +31,26 @@ import "./img/icon_footer_2_instagram.svg";
 import "./img/icon_footer_3_youtube.svg";
 import "./img/icon_footer_4_twitter.svg";
 import "./img/isotipo-ocensa.svg";
+import "./img/slider-home.jpg";
+import "./img/retos-img.jpg";
+import "./img/icon_ocenza_home_retos_que_nos_impulsan.svg"
+import "./img/imag-3.jpg";
+import "./img/imag-4.jpg";
+import "./img/imag-5.jpg";
+import "./img/imag-6.jpg";
+import "./img/imag-7.jpg";
+import "./img/imag-8.jpg";
+import "./img/icon_ocenza_home_linea_de_tiempo.svg";
+import "./img/icon_ocenza_home_componente_ambiental.svg";
+import "./img/imag-10.jpg";
+
+
+$(function(){
+    $('.ts-slick').slick({
+        prevArrow: `<button class='nav-left'><img src='${ArrowLeft}'></button>`,
+        nextArrow: `<button class='nav-right'><img src='${ArrowRight}'></button>`
+    });
+})
 
 menuToggle();
+
